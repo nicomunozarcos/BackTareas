70b9205469b8ecac3c378fee900b366ae5553858
Nico <nicomunozarcos@gmail.com>
2024-09-11 19:31:49 +0200

    variables de entorno

--- main/conexion.js
+++ main/conexion.js
@@ -2,11 +2,15 @@
 const mysql = require('mysql2');
 
 const database = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Kimono1717',
-    database: 'organizador_tareas',
-    port: '3306'
+    // host: '127.0.0.1',
+    // user: 'root',
+    // password: 'Kimono1717',
+    // database: 'organizador_tareas',
+    port: '3306',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }
 
 const pool = mysql.createPool(database);
